81989f6af12415a29c0d8f90a40735cd9e6c8f98
TEQUIE835 <david123orjuela@gmail.com>
2025-11-06 08:09:20 -0500

    uploading entities and dbcontext

---
+++ userManagement.Domain/Entities/Student.cs
@@ -0,0 +1,9 @@
+namespace userManagement.Domain.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateOnly BirthDate { get; set; } = new DateOnly();
+}
